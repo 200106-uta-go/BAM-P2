6da631395f1deeffa5ad957126827d0c12c58243
ubuntu <ajkim19@gmail.com>
2020-02-13 10:25:48 -0600

    Updating journal package
    Fixed some functions. Set up connections to Azure database.

--- pkg/journal/temp/main.go
+++ pkg/journal/temp/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
+	"log"
+	"strings"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
-type jEntry struct {
+type JEntry struct {
 	Date  string `json:"date"`
 	Entry string `json:"entry"`
 }
@@ -20,100 +26,112 @@ var (
 	jTableID    int      // Temporary reference to id value of table journal_entries
 	jTableDate  string   // Temporary reference to date value of table journal_entries
 	jTableEntry string   // Temporary reference to entry value of table journal_entries
-	jEntries    []jEntry // LIst of jEntry struct
+	JEntries    []JEntry // Slice of JEntry struct
 )
 
 func init() {
-	// **Need to alter database access for SQL Server or Postgres**
-	dataSourceName
+	var server = "<your_server.database.windows.net>"
+	var port = 1433
+	var user = "<your_username>"
+	var password = "<your_password>"
+	var database = "<your_database>"
 
-	// Makes a handle for the database journal
-	database, err = sql.Open("sqlite3", dataSourceName)
+	// Build connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;", server, user, password, port, database)
+
+	// Create connection pool
+	database, err = sql.Open("sqlserver", connString)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error creating connection pool: ", err.Error())
+	}
+	ctx := context.Background()
+	err = database.PingContext(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 }
 
-// Tentative plan for user authorization
-func main() {
-	/*
-		Implement acquisition methods for username and password
-	*/
-	usercheck := CheckUsername(database, username)
+// // Tentative plan for user authorization
+// func main() {
+// 	/*
+// 		Implement acquisition methods for username and password
+// 	*/
+// 	usercheck := CheckUsername(database, username)
 
-	if usercheck {
-		for {
-			passwordcheck := CheckPassword(database, username, password)
+// 	if usercheck {
+// 		for {
+// 			passwordcheck := CheckPassword(database, username, password)
 
-			if passwordcheck {
-				/*
-					Implementation of main functions
-				*/
-			} else {
-				/*
-					Implement condition if password is invalid
-				*/
-			}
-		}
-	} else {
-		for {
-			/*
-				Implement prompt and input method of new username
-			*/
-			checkUsernameChars := CheckInvalidChars(username)
-			if checkUsernameChars {
-				for {
-					checkPasswordChars := CheckInvalidChars(password)
-					if checkPasswordChars {
-						AddUsernamePassword(database, username, password)
-						break
-					} else {
-						/*
-							Implement condition if password is invalid
-						*/
-					}
-				}
-				break
-			} else {
-				/*
-					Implement condition if username is invalid
-				*/
-			}
-		}
-	}
+// 			if passwordcheck {
+// 				/*
+// 					Implementation of main functions
+// 				*/
+// 			} else {
+// 				/*
+// 					Implement condition if password is invalid
+// 				*/
+// 			}
+// 		}
+// 	} else {
+// 		for {
+// 			/*
+// 				Implement prompt and input method of new username
+// 			*/
+// 			checkUsernameChars := CheckInvalidChars(username)
+// 			if checkUsernameChars {
+// 				for {
+// 					checkPasswordChars := CheckInvalidChars(password)
+// 					if checkPasswordChars {
+// 						AddUsernamePassword(database, username, password)
+// 						break
+// 					} else {
+// 						/*
+// 							Implement condition if password is invalid
+// 						*/
+// 					}
+// 				}
+// 				break
+// 			} else {
+// 				/*
+// 					Implement condition if username is invalid
+// 				*/
+// 			}
+// 		}
+// 	}
 
-	// Creates the username table if it does not exist
-	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS ? (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	statement.Exec(username)
-}
+// 	// Creates the username table if it does not exist
+// 	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS ? (id INTEGER PRIMARY KEY, username TEXT, password TEXT)")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	statement.Exec(username)
+// }
 
-// CheckUsername checks for existence of username in user_table
-func CheckUsername(db *sql.DB, un string) bool {
-	rows, err := db.Query(`SELECT * FROM ?`, un)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
+// // CheckUsername checks for existence of username in user_table
+// func CheckUsername(db *sql.DB, un string) bool {
+// 	rows, err := db.Query(`SELECT * FROM ?`, un)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer rows.Close()
 
-	for rows.Next() {
-		err := rows.Scan(&jTableID, &jTableDate, &jTableEntry)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if uTableUN == un {
-			return true
-		}
-	}
-}
+// 	for rows.Next() {
+// 		err := rows.Scan(&jTableID, &jTableDate, &jTableEntry)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		if uTableUN == un {
+// 			return true
+// 		}
+// 	}
+// }
 
-// CheckInvalidUNChars checks username for the existence of special characters 
+// CheckInvalidUNChars checks username for the existence of special characters
 func CheckInvalidUNChars(un string) bool {
 	if strings.ContainsAny(un, " !@#$%^&()[]{}`~:;<>,./\\+*\"?'") == false {
 		return false
 	}
+	return true
 }
 
 // CheckInvalidPSChars checks password for the existence of special characters
@@ -121,9 +139,10 @@ func CheckInvalidPSChars(un string) bool {
 	if strings.ContainsAny(un, " ()[]{}~:;<>,./\\+\"'") == false {
 		return false
 	}
+	return true
 }
 
-// AddUsername adds the username to user_table
+// AddUsernamePassword adds the username to user_table
 func AddUsernamePassword(db *sql.DB, un string, ps string) {
 	statement, err := database.Prepare(`INSERT INTO ? (username, password) VALUES (?, ?)`)
 	if err != nil {
@@ -149,17 +168,7 @@ func CheckPassword(db *sql.DB, un string, ps string) bool {
 			return true
 		}
 	}
-}
-
-// InputEntry adds the current date as a string and prompts the user for
-// a journal entry input to be stored into the database in association
-// with the date.
-func InputEntry(db *sql.DB, un string, jEntry string) {
-	jDate := string(time.Now().Format("2006-01-02"))
-
-	ifEntryExists(db, un, jEntry, jDate)
-
-	printEntry(db, jDate)
+	return false
 }
 
 // EditEntry replaces the journal entry of a particular date
@@ -169,11 +178,10 @@ func EditEntry(db *sql.DB, un string, jDate string, jEntry string) {
 		log.Fatal(err)
 	}
 	defer statement.Close()
-	defer return
 	statement.Exec(un, jEntry, jDate)
 }
 
-// ifEntryExists checks to see if an entry for a certain date already exists
+// IfEntryExists checks to see if an entry for a certain date already exists
 // in the username table in a specified SQL database.
 func IfEntryExists(db *sql.DB, un string, jEntry string, jDate string) {
 	rows, err := db.Query(`SELECT * FROM ? WHERE date = ?`, un, jDate)
@@ -182,14 +190,12 @@ func IfEntryExists(db *sql.DB, un string, jEntry string, jDate string) {
 	}
 	defer rows.Close()
 
-	dateExists := false
-
 	for rows.Next() {
 		err := rows.Scan(&jTableID, &jTableDate, &jTableEntry)
 		if err != nil {
 			log.Fatal(err)
 		}
-		// If the date of the entry already exists, the entry will be added	to
+		// If the date of the entry already exists, the entry will be appended	to
 		// the preexisting entry after a new line.
 		if jTableDate == jDate {
 			rows, err = db.Query("SELECT * FROM ? WHERE date = ?", un, jDate)
@@ -205,33 +211,34 @@ func IfEntryExists(db *sql.DB, un string, jEntry string, jDate string) {
 			}
 
 			EditEntry(db, un, jDate, jEntry)
-			return
 
+			// If the date of the entry does not exist, the entry will be added to
+			// the added to the table.
 		} else {
 			statement, err := db.Prepare("INSERT INTO ? (date, entry) VALUES (?, ?)")
 			if err != nil {
 				log.Fatal(err)
 			}
 			defer statement.Close()
-			defer return
 			statement.Exec(un, jDate, jEntry)
 		}
 	}
+	return
 }
 
 // OutputJournal returns entire journal of username in []jEntry format
-func OutputJournal(un string) []jEntry {
-	rows, err = database.Query("SELECT * FROM ? ORDER BY date DESC", un)
+func OutputJournal(un string) []JEntry {
+	rows, err := database.Query("SELECT * FROM ? ORDER BY date DESC", un)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	jEntries = []jEntry{}
+	JEntries = []JEntry{}
 
 	for rows.Next() {
-		rows.Scan(&dbid, &dbdate, &dbentry)
-		jEntries = append(jEntries, jEntry{Date: dbdate, Entry: dbentry})
+		rows.Scan(&jTableID, &jTableDate, &jTableEntry)
+		JEntries = append(JEntries, JEntry{Date: jTableDate, Entry: jTableEntry})
 	}
 
-	return jEntries
+	return JEntries
 }
