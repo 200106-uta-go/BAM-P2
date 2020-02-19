16a245d689ad5524b09be96ee478b243c8abb78f
ubuntu <ajkim19@gmail.com>
2020-02-18 21:16:19 -0600

    Adding controller functions

--- cmd/controller/controller.go
+++ cmd/controller/controller.go
@@ -3,40 +3,122 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"strings"
 )
 
-var subcommand string
+// var subcommand string
 
-func init() {
-	//gets the subcommand passed
-	if len(os.Args) > 0 {
-		subcommand = os.Args[1]
+/*
+// Control struct outline
+type Control struct {
+	Apply -f -- filepath string
+	Delete ${object} name  -- object string, name string
+	Get ${object} name  -- object string, name string
+	Describe   -- object string, name string
+	Scale -- replicascount string, deployment filepath string
+	Logs -- podname string
+	Cluster-info -- no arguments
+}
+*/
+
+// func init() {
+// 	//gets the subcommand passed
+// 	if len(os.Args) > 0 {
+// 		subcommand = os.Args[1]
+// 	}
+// }
+
+// //calls controller functions based on cli args
+// func main() {
+// 	switch subcommand {
+// 	case "scale":
+// 		scale(os.Args[2], "deploy.yaml")
+// 	}
+// }
+
+// func cmdRun(s string) {
+// 	mySlice := strings.Split(s, " ")
+// 	cmd := exec.Command(mySlice[0], mySlice[1:]...)
+// 	fmt.Println(cmd)
+// 	err := cmd.Run()
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// }
+
+// //scales the deployed pods
+// func scale(num string, deploy string) {
+// 	fmt.Println("Scaling to", num)
+// 	cmdRun(fmt.Sprintf("kubectl scale --replicas %s -f %s", num, deploy))
+// }
+
+// KubeApply creates string for kubectl apply command
+func KubeApply(filepath string) string {
+	if filepath == "" {
+		return ""
+	} else {
+		return fmt.Sprintf("apply -f %s", filepath)
 	}
 }
 
-//calls controller functions based on cli args
-func main() {
-	switch subcommand {
-	case "scale":
-		scale(os.Args[2], "deploy.yaml")
+// KubeDelete creates string for kubectl delete command
+func KubeDelete(object string, name string) string {
+	if object == "" || name == "" {
+		return ""
+	} else {
+		return fmt.Sprintf("delete %s %s", object, name)
 	}
 }
 
-func cmdRun(s string) {
-	mySlice := strings.Split(s, " ")
-	cmd := exec.Command(mySlice[0], mySlice[1:]...)
-	fmt.Println(cmd)
+// KubeGet creates string for kubectl get command
+func KubeGet(object string, name string) string {
+	if object == "" || name == "" {
+		return ""
+	} else {
+		return fmt.Sprintf("get %s %s", object, name)
+	}
+}
+
+// KubeDescribe creates string for kubectl describe command
+func KubeDescribe(object string, name string) string {
+	if object == "" || name == "" {
+		return ""
+	} else {
+		return fmt.Sprintf("describe %s %s", object, name)
+	}
+}
+
+// KubeScale creates string for kubectl scale command
+func KubeScale(count string, deployment string) string {
+	if count == "" || deployment == "" {
+		return ""
+	} else if strings.HasSuffix(deployment, ".yaml") {
+		return fmt.Sprintf("scale --replicas=%s -f %s", count, deployment)
+	} else {
+		return fmt.Sprintf("scale --replicas=%s %s", count, deployment)
+	}
+}
+
+// KubeLogs creates string for kubectl logs command
+func KubeLogs(podname string) string {
+	if podname == "" {
+		return ""
+	} else {
+		return fmt.Sprintf("logs %s", podname)
+	}
+}
+
+// KubeClusterInfo creates string for kubectl cluster-info command
+func KubeClusterInfo() string {
+	return "cluster-info"
+}
+
+// KubeCommand runs the kubectl CLI with a command
+func KubeCommand(command string) {
+	cmd := exec.Command(fmt.Sprintf("kubectl %s", command))
 	err := cmd.Run()
 	if err != nil {
 		log.Println(err)
 	}
-}
-
-//scales the deployed pods
-func scale(num string, deploy string) {
-	fmt.Println("Scaling to", num)
-	cmdRun(fmt.Sprintf("kubectl scale --replicas %s -f %s", num, deploy))
 }
