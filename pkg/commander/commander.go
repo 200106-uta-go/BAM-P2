3a1730db02e61c9a7741f04d164c5c1d8744335b
Gamemastertwig <brandonlocker@hotmail.com>
2020-02-20 13:33:39 -0600

    modified commander pkg

--- pkg/commander/commander.go
+++ pkg/commander/commander.go
@@ -1,21 +1,23 @@
+// Package commander is a wrapper package for the "os/exec" .Command function allowing diffrent types of returns and user controls
 package commander
 
 import (
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"strings"
 )
 
+// cmd is a private function that is aonly available to this pkg but is used to send the command to the OS
 func cmd(s string) ([]byte, error) {
 	cmdSlice := strings.Split(s, " ")
 	command := exec.Command(cmdSlice[0], cmdSlice[1:]...)
-	command.Stderr = os.Stderr
+	//command.Stderr = os.Stderr
 	out, err := command.Output()
 	return out, err
 }
 
+// CmdRun runs a command provided by user (string) and provides feedback on what it is doing
 func CmdRun(s string) {
 	fmt.Println("Running command: " + s)
 	out, err := cmd(s)
@@ -25,6 +27,14 @@ func CmdRun(s string) {
 	fmt.Println(string(out))
 }
 
+// CmdRunNoErrs runs a command provided by user (string) and provides feedback on what it is doing but without showing errors
+func CmdRunNoErr(s string) {
+	fmt.Println("Running command: " + s)
+	out, _ := cmd(s)
+	fmt.Println(string(out))
+}
+
+// CmdRunSilent runs a command provided by user (string) and does not provide feedback on what it is doing
 func CmdRunSilent(s string) {
 	_, err := cmd(s)
 	if err != nil {
@@ -32,6 +42,13 @@ func CmdRunSilent(s string) {
 	}
 }
 
+// CmdRunSilentNoErr runs a command provided by user (string) and does not provide feedback on what it is doing
+// and without showing any errors
+func CmdRunSilentNoErr(s string) {
+	cmd(s)
+}
+
+// CmdRunOut runs a command provided by user (string) and returns the output in a string
 func CmdRunOut(s string) string {
 	fmt.Println("Running command: " + s)
 	out, err := cmd(s)
@@ -42,10 +59,27 @@ func CmdRunOut(s string) string {
 	return string(out)
 }
 
+// CmdRunOutNoErr runs a command provided by user (string) and returns the output in a string but with out showing errors
+func CmdRunOutNoErr(s string) string {
+	fmt.Println("Running command: " + s)
+	out, _ := cmd(s)
+	fmt.Println(string(out))
+	return string(out)
+}
+
+// CmdRunOutSilent runs a command provided by user (string) and returns the output in a string and does not provide feedback
+// on what it is doing
 func CmdRunOutSilent(s string) string {
 	out, err := cmd(s)
 	if err != nil {
 		log.Println("Command: " + s + ": Failed :: " + err.Error())
 	}
+	return string(out)
+}
+
+// CmdRunOutSilent runs a command provided by user (string) and returns the output in a string and does not provide feedback
+// on what it is doing but with out show
+func CmdRunOutSilentNoErr(s string) string {
+	out, _ := cmd(s)
 	return string(out)
 }
