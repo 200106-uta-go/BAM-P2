110ccfe364894efc8735b6ba1eeec73850859924
mattackard <mattackard@gmail.com>
2020-02-20 11:27:19 -0600

    bugfix kubectl version

--- deployments/kubectlinit/kubectlinit.go
+++ deployments/kubectlinit/kubectlinit.go
@@ -11,7 +11,7 @@ import (
 // CheckInstall simply checks to see if kops is installed, returns true if installled, otherwise false.
 func CheckInstall() bool {
 	// logic to check if kops is already installed
-	version := commander.CmdRunOutSilent("kubectl version")
+	version := commander.CmdRunOutSilent("kubectl version --client --short")
 	v := strings.Split(version, " ")
 	if v[0] == "Client" {
 		return true
